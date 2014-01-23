01773716c1bd3b84781f305044558540d061acd4
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2014-01-23 17:55:35 +0530

    Files added

---
+++ CallerInfoSample/Logger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using log4net;
+
+namespace CallerInfoSample
+{
+    public class Logger
+    {
+        private static Logger _logger;
+        private ILog _log;
+        public static Logger Instance
+        {
+            get
+            {
+                if (_logger == null)
+                {
+                    _logger = new Logger();
+                }
+                return _logger;
+            }
+        }
+
+        private Logger()
+        {
+        }
+
+        private void Initialize()
+        {
+            _log = LogManager.GetLogger(typeof(Logger));
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        public void Error(Exception ex, 
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if (_log == null)
+            {
+                Initialize();
+            }
+            _log.Error(string.Format(
+                "Error Logged From method {0} in file {1} at line {2}",
+                memberName, sourceFilePath, sourceLineNumber), ex);
+        }
+
+        public void Warning(Exception ex,                    
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if (_log == null)
+            {
+                Initialize();
+            }
+            _log.Warn(string.Format("Warning from method {0} in file {1} at line {2}", 
+                memberName, sourceFilePath, sourceLineNumber), ex);
+        }
+
+        public void Debug(Exception ex,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            if (_log == null)
+            {
+                Initialize();
+            }
+            _log.Debug(string.Format(
+                "Debug entry from method {0} in file {1} at line {2}",
+                memberName, sourceFilePath, sourceLineNumber), ex);
+        }
+    }
+}
---
+++ CallerInfoSample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallerInfoSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopInfinitely();
+        }
+
+        private static void LoopInfinitely()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo keyCode = Console.ReadKey();
+                Console.WriteLine(keyCode.Key.ToString());
+                if (keyCode.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                else
+                {
+                    if (keyCode.Key >= ConsoleKey.F1 && 
+                        keyCode.Key <= ConsoleKey.F10)
+                    {
+                        Logger.Instance.Warning(new
+                            ApplicationException("Warning: You pressed keycode *" +
+                            keyCode.Key.ToString() + "* Exception"));
+                    }
+                    else if (keyCode.Key >= ConsoleKey.D0 && 
+                        keyCode.Key <= ConsoleKey.D9)
+                    {
+                        Logger.Instance.Debug(new
+                            ApplicationException("Debug: You pressed keycode *" +
+                            keyCode.Key.ToString() + "* Exception"));
+                    }
+                    else
+                    {                        
+                        LogError(keyCode);
+                    }
+                }
+            }
+        }
+
+        private static void LogError(ConsoleKeyInfo keyCode)
+        {
+            Logger.Instance.Error(new
+                           ApplicationException("ERROR: You pressed keycode *" +
+                           keyCode.Key.ToString() + "* Exception"));
+        }
+    }
+}
